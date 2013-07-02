5eab4f7e323721345c90220ff98c9a542cccc52a
Steffen Ritter <info@rs-websystems.de>
2013-07-02 12:01:08 +0200

    [TASK] Remove unused stuff from table

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,13 +4,6 @@
 CREATE TABLE tx_upgradereport_domain_model_issue (
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
 
 	inspection varchar(255) DEFAULT '' NOT NULL,
 	issue_identifier varchar(255) DEFAULT '' NOT NULL,
