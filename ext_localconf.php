ff13f68bf06e1c1c84a97e8cde820c820f28c529
Steffen Ritter <info@rs-websystems.de>
2013-07-07 17:01:00 +0200

    [TASK] Register CLI Script

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,6 +11,11 @@ $checkArray = array(
 Tx_Upgradereport_Service_Check_Registry::getInstance()->registerChecks($checkArray);
 
 
+$TYPO3_CONF_VARS['SC_OPTIONS']['GLOBAL']['cliKeys']['upgradereport'] = array(
+	t3lib_extMgm::extPath('upgradereport', 'Classes/Cli/class.upgradereport_cli.php'),
+	'_CLI_upgradereport'
+);
+
 $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['NotExistingClass'] = 'Tx_Upgradereport_Controller_AbstractModuleController';
 
 ?>
