68e211a1e5307f11b1ed6ac736a7bcf24e91fdd2
Michiel Roos <michiel@maxserv.nl>
2013-08-29 21:53:59 +0200

    [TASK] Cleanup Checks Definitions * Move default Definition methods to Tx_Smoothmigration_Checks_AbstractCheckDefinition

--- Classes/Checks/Database/Utf8/Definition.php
+++ Classes/Checks/Database/Utf8/Definition.php
@@ -66,15 +66,6 @@ class Tx_Smoothmigration_Checks_Core_Xclasses_Definition extends Tx_Smoothmigrat
 		return Tx_Smoothmigration_Domain_Interface_CheckDescription::TYPE_PHP_CODE;
 	}
 
-	/**
-	 * Return the maximal TYPO3 Version the checks needs to be executed on.
-	 *
-	 * @return string
-	 */
-	public function getMaximalTypo3Version() {
-		return '4.7.999';
-	}
-
 }
 
 ?>
