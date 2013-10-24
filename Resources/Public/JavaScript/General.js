7974db1fdb8514762e792cf8aa8dcd8fc1b9ab73
Michiel Roos <michiel@maxserv.nl>
2013-10-24 10:33:58 +0200

    [TASK] Cleanup JavaScript and module index
    * Cleaned up JavaScript to please Google Closure compiler
    * Moved spinners with labels to Index.html so they may be localized
    * Factor out the message status switching to a function

--- Resources/Public/JavaScript/General.js
+++ Resources/Public/JavaScript/General.js
@@ -1,8 +1,6 @@
-var UpgradeReport = function() {
+(function($) {
     'use strict';
-    var self = this;
-
-    self._runTest = function(testIdentifier) {
+    function runTest (testIdentifier) {
         var $listElement = $('li[data-checkid=' + testIdentifier + ']');
         $.ajax({
             type: 'POST',
@@ -16,40 +14,22 @@ var UpgradeReport = function() {
                 }
             }
         }).done(function(result) {
-            $listElement.find('.spinner').remove();
-            $listElement.find('.feedback').html('<p>Test Run ' + result.result + '.</p>');
+            $listElement.find('.feedback .running').hide();
+            $listElement.find('.feedback .report').html('<p>Test Run ' + result.result + '.</p>');
             if (result.result === 'OK') {
-                $listElement.find('.feedback').html('<p>Found ' + result.issueCount + ' issues.</p>');
+                $listElement.find('.feedback .report').html('<p>Found ' + result.issueCount + ' issues.</p>');
                 if (result.issueCount > 0) {
-                    $listElement
-                        .removeClass('message-error')
-                        .removeClass('message-notice')
-                        .removeClass('message-ok')
-                        .addClass('message-warning');
-                /*    $content.append('<ul></ul>')
-                    var $list = $content.children('ul');
-                    for (var i = 0; i < result.issueCount; i++) {
-                        $list.append('<li><p class="explenation">' + result.issues[i].explenation + '</p><p class="solution">' + result.issues[i].solution + '</p></li>')
-                    }
-                */
+                    $listElement.setMessageState('warning');
                 } else {
-                    $listElement
-                        .removeClass('message-error')
-                        .removeClass('message-notice')
-                        .removeClass('message-warning')
-                        .addClass('message-ok');
+                    $listElement.setMessageState('ok');
                 }
             } else {
-                $listElement
-                    .removeClass('message-notice')
-                    .removeClass('message-ok')
-                    .removeClass('message-warning')
-                    .addClass('message-error');
+                $listElement.setMessageState('error');
             }
         });
-    };
+    }
 
-    self._clearTestResults = function(testIdentifier) {
+    function clearTestResults (testIdentifier) {
         var $listElement = $('li[data-checkid=' + testIdentifier + ']');
         $.ajax({
             type: 'POST',
@@ -63,62 +43,74 @@ var UpgradeReport = function() {
                 }
             }
         }).done(function(result) {
-            $listElement.find('.spinner').remove();
-            $listElement.find('.feedback').html('<p>Test Run ' + result.result + '.</p>');
+            $listElement.find('.feedback .clearing').hide();
+            $listElement.find('.feedback .report').html('<p>Test Run ' + result.result + '.</p>');
             if (result.result === 'OK') {
-                $listElement.find('.feedback').html('<p>Cleared ' + result.issueCount + ' issues.</p>');
-                $listElement
-                    .removeClass('message-error')
-                    .removeClass('message-notice')
-                    .removeClass('message-warning')
-                    .addClass('message-ok');
+                $listElement.find('.feedback .report').html('<p>Cleared ' + result.issueCount + ' issues.</p>');
+                $listElement.setMessageState('ok');
             } else {
-                $listElement
-                    .removeClass('message-notice')
-                    .removeClass('message-ok')
-                    .removeClass('message-warning')
-                    .addClass('message-error');
+                $listElement.setMessageState('error');
             }
         });
-    };
+    }
 
+    $.extend($.fn, {
+        clearIssues: function() {
+            this.bind('click.clearIssues', function() {
+                $(this).parents('li').find('.feedback .running').hide();
+                $(this).parents('li').find('.feedback .clearing').show();
+                clearTestResults($(this).parents('li').attr('data-checkid'));
+            });
+            return this;
+        },
 
-    self.click_runButton = function(event) {
-        var $link = $(this);
-        var checkId = $link.parents('li').attr('data-checkid');
-        $link.parents('li').find('.feedback').html('<span class="spinner"><!-- --></span><strong class="spinner"> Running...</strong>');
-        self._runTest(checkId);
-        return false;
-    };
+        clearAllIssues: function() {
+            this.bind('click.clearAllIssues', function() {
+                $('li[data-checkid]').each(function(index, element) {
+                    $(element).find('.feedback .running').hide();
+                    $(element).find('.feedback .clearing').show();
+                    clearTestResults($(element).attr('data-checkid'));
+                });
+            });
+            return this;
+        },
 
-    self.click_clearIssues = function(event) {
-        var $link = $(this);
-        var checkId = $link.parents('li').attr('data-checkid');
-        $link.parents('li').find('.feedback').html('<span class="spinner"><!-- --></span><strong class="spinner"> Clearing...</strong>');
-        self._clearTestResults(checkId);
-        return false;
-    };
+        runCheck: function() {
+            this.bind('click.runCheck', function() {
+                $(this).parents('li').find('.feedback .clearing').hide();
+                $(this).parents('li').find('.feedback .running').show();
+                runTest($(this).parents('li').attr('data-checkid'));
+            });
+            return this;
+        },
 
-    self.click_runAllButton = function(event) {
-        $('li[data-checkid]').each(function(index, element) {
-            $(element).find('.feedback').html('<span class="spinner"><!-- --></span><strong class="spinner"> Running...</strong>');
-            self._runTest($(element).attr('data-checkid'));
-        });
-    };
+        runAllChecks: function() {
+            this.bind('click.runAllChecks', function() {
+                $('li[data-checkid]').each(function(index, element) {
+                    $(element).find('.feedback .clearing').hide();
+                    $(element).find('.feedback .running').show();
+                    runTest($(element).attr('data-checkid'));
+                });
+            });
+            return this;
+        },
 
-    self.click_clearAllIssuesButton = function(event) {
-        $('li[data-checkid]').each(function(index, element) {
-            $(element).find('.feedback').html('<span class="spinner"><!-- --></span><strong class="spinner"> Clearing...</strong>');
-            self._clearTestResults($(element).attr('data-checkid'));
-        });
-    };
-};
+        setMessageState: function(state) {
+            this
+                .removeClass('message-error')
+                .removeClass('message-notice')
+                .removeClass('message-ok')
+                .removeClass('message-warning')
+                .addClass('message-' + state);
+    }
+
+    });
+})(jQuery);
 
 jQuery(document).ready(function($) {
     'use strict';
-    var functionality = new UpgradeReport();
-    $('.t3-smoothmigration-runCheck').click(functionality.click_runButton);
-    $('.t3-smoothmigration-clearIssues').click(functionality.click_clearIssues);
-    $('.t3-smoothmigration-runAllChecks').click(functionality.click_runAllButton);
-    $('.t3-smoothmigration-clearAllIssues').click(functionality.click_clearAllIssuesButton);
+    $('.t3-smoothmigration-clearIssues').clearIssues();
+    $('.t3-smoothmigration-clearAllIssues').clearAllIssues();
+    $('.t3-smoothmigration-runCheck').runCheck();
+    $('.t3-smoothmigration-runAllChecks').runAllChecks();
 });
